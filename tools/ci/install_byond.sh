7a69edf6aad96c6cd9e19f36462c7b591c29434b
cassy_jenelle <intelligenery@live.com>
2025-07-25 20:22:21 +0100

    Update install_byond.sh

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -14,7 +14,7 @@ else
   rm -rf "$HOME/BYOND"
   mkdir -p "$HOME/BYOND"
   cd "$HOME/BYOND"
-  curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+  curl "https://indm.dev/byond/${{ env.BYOND_MAJOR }}/${{ env.BYOND_MAJOR }}.${{ env.BYOND_MINOR }}_byond_linux.zip" -o byond.zip
   unzip byond.zip
   rm byond.zip
   cd byond
